539f17e0e04913fa4d645094adb0fc895e651e6c
jlauros <janne.lauros@csc.fi>
2024-01-11 15:10:03 +0200

    Renamed

---
+++ oauth2-devicegrant-impl/src/main/java/fi/csc/shibboleth/plugin/oauth2/profile/impl/ValidateExpectedGrantType.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2019-2024 CSC- IT Center for Science, www.csc.fi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fi.csc.shibboleth.plugin.oauth2.profile.impl;
+
+import java.util.Set;
+import javax.annotation.Nonnull;
+
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.nimbusds.oauth2.sdk.GrantType;
+import com.nimbusds.oauth2.sdk.ParseException;
+
+import fi.csc.shibboleth.plugin.oauth2.messaging.impl.OAuth2DeviceTokenRequest;
+import net.shibboleth.idp.profile.AbstractProfileAction;
+import net.shibboleth.oidc.metadata.context.OIDCMetadataContext;
+import net.shibboleth.oidc.profile.core.OidcEventIds;
+import net.shibboleth.shared.logic.Constraint;
+
+/**
+ * An action that validates expected grant type is registered for the relying
+ * party. For example in Device Flow the grant type is expected to be
+ * urn:ietf:params:oauth:grant-type:device_code. Action does not check the
+ * existence or value of the grant in the actual request.
+ */
+public class ValidateExpectedGrantType extends AbstractProfileAction {
+
+    /** Class logger. */
+    @Nonnull
+    private Logger log = LoggerFactory.getLogger(ValidateExpectedGrantType.class);
+
+    @Nonnull
+    private String expectedGrantType = OAuth2DeviceTokenRequest.grantTypeValue;
+
+    /** OIDC Metadata context. */
+    private OIDCMetadataContext oidcMetadataContext;
+
+    /**
+     * Set expected grant type.
+     * 
+     * @param type expected grant type
+     */
+    public void setExpectedGrantType(@Nonnull String type) {
+        Constraint.isNotEmpty(type, "Expected grant type must not be null or empty");
+        expectedGrantType = type;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+
+        if (!super.doPreExecute(profileRequestContext)) {
+            return false;
+        }
+        if (profileRequestContext.getInboundMessageContext() == null) {
+            log.error("{} No inbound message context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            return false;
+        }
+        oidcMetadataContext = profileRequestContext.getInboundMessageContext().getSubcontext(OIDCMetadataContext.class);
+        if (oidcMetadataContext == null) {
+            log.error("{} No metadata found for relying party", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        final Set<GrantType> registeredTypes = oidcMetadataContext.getClientInformation().getMetadata().getGrantTypes();
+        try {
+            if (registeredTypes == null || registeredTypes.isEmpty()
+                    || !registeredTypes.contains(GrantType.parse(expectedGrantType))) {
+                log.error("{} The grant type {} is not registered for this RP", getLogPrefix(), expectedGrantType);
+                ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT_TYPE);
+            }
+        } catch (ParseException e) {
+            log.error("{} Unable to parse grant type from {}", getLogPrefix(), expectedGrantType);
+            ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT_TYPE);
+        }
+    }
+}
